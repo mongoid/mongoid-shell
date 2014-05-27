da835e423fe61f79f96a2c8f8af0d39f62e8a809
dblock <dblock@dblock.org>
2014-05-27 09:35:11 -0400

    Fix: wrong condition in username/password.

--- lib/mongoid/shell/properties/username.rb
+++ lib/mongoid/shell/properties/username.rb
@@ -13,7 +13,7 @@ module Mongoid
             else
               node = session.cluster.nodes.first
               raise Mongoid::Shell::Errors::SessionNotConnectedError unless node
-              return nil unless node.credentials.key?(db) || node.credentials[db].empty?
+              return nil if !node.credentials.key?(db) || node.credentials[db].empty?
               node.credentials[db][0]
             end
           end
