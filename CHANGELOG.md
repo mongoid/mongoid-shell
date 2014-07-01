58e875ed7a3ee457757ab32910820ee8f7de10c5
dblock <dblock@dblock.org>
2014-07-01 19:15:07 -0400

    Preparing for next development iteration, 0.3.1.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+Next
+====
+
+* Your contribution here.
+
 0.3.0 (7/1/2014)
 ================
 
--- lib/mongoid/shell/version.rb
+++ lib/mongoid/shell/version.rb
@@ -1,5 +1,5 @@
 module Mongoid
   module Shell
-    VERSION = '0.3.0'
+    VERSION = '0.3.1'
   end
 end
